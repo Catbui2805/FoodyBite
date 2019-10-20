d2b039d16ce62c27f585739b170647a69f61b5d4
Nguyen Tran Cong <Catbui2805@gmail.com>
2019-10-21 00:20:54 +0700

    init project foody bite

---
+++ Foody Bite/Foody Bite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foody Bite
+//
+//  Created by Nguyen Tran Cong on 10/21/19.
+//  Copyright © 2019 Nguyen Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
